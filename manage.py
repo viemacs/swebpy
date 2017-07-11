1b28dfed694322b0b26d4b38c9192c730d2a6339
viemacs <emacguo@gmail.com>
2017-07-11 18:06:21 +0800

    remove duplicated codes from manage

--- manage.py
+++ manage.py
@@ -9,9 +9,6 @@ Manage the applicationn
 
 import os, sys
 import click
-from werkzeug import run_simple
-
-from werkzeug.wrappers import Request, Response
 
 def create():
     from sqlalchemy import Table
@@ -19,30 +16,6 @@ def create():
     for (name, table) in vars(model).items():
         if isinstance(table, Table):
             table.create()
-
-def run():
-    pass
-
-def x_run():
-    import router
-    if os.environ.get('REQUEST_METHOD', ''):
-        # from wsgiref.handlers import BaseCGIHandler
-        # BaseCGIHandler(sys.stdin, sys.stdout, sys.stderr, os.environ).run(router.urls)
-        pass
-    else:
-        from werkzeug.wrappers import Request, Response
-        from werkzeug.serving import run_simple
-
-        @Request.application
-        def app(request):
-            return Response('hello there')
-
-        run_simple('', 4000, app)
-        return
-
-        httpd.set_app()
-        print('Serving HTTP on %s port %s ...' % httpd.socket.getsockname())
-        httpd.serve_forever()
 
 # def make_app():
 #     from application import make_app
@@ -62,15 +35,30 @@ def cli():
 @click.option('--processes', type=int, default=1, help='1')
 def runserver(hostname, port, reloader, debugger, evalex, threaded, processes):
     '''start a server'''
-
     # app = make_app()
 
-    @Request.application
-    def app(request):
-        return Response('foo')
-    run_simple(hostname, port, app,
+    if os.environ.get('REQUEST_METHOD', ''):
+        import router
+        # from wsgiref.handlers import BaseCGIHandler
+        # BaseCGIHandler(sys.stdin, sys.stdout, sys.stderr, os.environ).run(router.urls)
+        pass
+    else:
+        from werkzeug.wrappers import Request, Response
+        from werkzeug.serving import run_simple
+
+        @Request.application
+        def app(request):
+            return Response('hello there')
+
+        # run_simple(hostname, port, app,
+        run_simple('', 4000, app,
                    use_reloader=reloader, use_debugger=debugger, use_evalex=evalex,
                    threaded=threaded, processes=processes)
+
+        return
+        httpd.set_app()
+        print('Serving HTTP on %s port %s ...' % httpd.socket.getsockname())
+        httpd.serve_forever()
 
 
 if __name__ == '__main__':
